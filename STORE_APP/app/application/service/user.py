37e55898903dbfcae0e22e1135afb950fe3b901c
Heechan Kang <weston0713@gmail.com>
2022-09-12 13:06:42 +0900

    feat: user 생성로직 보완

--- STORE_APP/app/application/service/user.py
+++ STORE_APP/app/application/service/user.py
@@ -8,6 +8,9 @@ class UserService:
 
     def create_user(self, user_name: str):
         _user = User(name=user_name)
+        # DB에 해당 이름과 같은 이름이 있는지 확인하고, 있으면 예외처리
+        if self.repository.find_one(model=_user):
+            raise ValueError("유저가 이미 존재합니다.")
         user = self.repository.create(_user)
 
         return user
