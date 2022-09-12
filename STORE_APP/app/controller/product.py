4756c63b672cf6f0cad0c8f5357a85bcf6bfda43
Heechan Kang <weston0713@gmail.com>
2022-09-12 14:40:58 +0900

    feat: product_service 추가

---
+++ STORE_APP/app/controller/product.py
@@ -0,0 +1,13 @@
+from fastapi import HTTPException
+
+from app.infrastructure.database.repository.product import ProductRepository
+from app.application.service.product import ProductService
+
+def find_product(product_id: int):
+    repository = ProductRepository()
+    product_service = ProductService(repository=repository)
+    try:
+        product = product_service.get_product(product_id=product_id)
+        return product
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
